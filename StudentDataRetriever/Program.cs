b471146d1527bf3d1fbb85035c39cb27899e2271
rajak0549 <rajak0549@gmail.com>
2023-10-24 16:27:42 +0530

    Add project files.

---
+++ StudentDataRetriever/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataRetriever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string s = Directory.GetCurrentDirectory();
+            Console.WriteLine(s);
+            try
+            {
+                string filePath = s + "\\Student_data.txt";
+
+
+                FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read);
+
+                StreamReader sr = new StreamReader(fs);
+                string data = sr.ReadToEnd();
+                Console.WriteLine(data);
+
+
+
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
